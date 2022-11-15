1aa6ebaf7c3f0b44ec60a1277caf0c7377ec17d1
Rehaman-khan github <rehamankhan1603@gmail.comgit>
2022-11-15 21:52:26 +0530

    initializing object by method

---
+++ com/cg/classandobject/Student2.java
@@ -0,0 +1,21 @@
+package com.cg.classandobject;
+
+public class Student2 {
+	int rollno;
+
+	String name;
+
+	void insertRecord(int r, String n) {
+
+	rollno = r;
+
+	name = n;
+
+	}
+
+	void displayInformation() {
+
+	System.out.println(rollno + " " + name);
+
+	}
+}
