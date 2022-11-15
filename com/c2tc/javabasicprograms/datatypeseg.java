3c70bf5fc19d4ceffd83b082f45ca8b4db90b1aa
Rehaman-khan github <rehamankhan1603@gmail.comgit>
2022-11-15 20:53:22 +0530

    datatypes example 3

---
+++ com/c2tc/javabasicprograms/datatypeseg.java
@@ -0,0 +1,12 @@
+package com.c2tc.javabasicprograms;
+
+public class datatypeseg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name="Rehman";
+		System.out.println("hello"+ " "+name);
+
+	}
+
+}
