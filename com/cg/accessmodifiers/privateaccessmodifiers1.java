d1c768173da248520722cd4ea1278f51935ff695
Rehaman-khan github <rehamankhan1603@gmail.comgit>
2022-11-16 16:26:11 +0530

    static keyword

--- com/cg/accessmodifiers/privateaccessmodifiers1.java
+++ com/cg/accessmodifiers/privateaccessmodifiers1.java
@@ -1,5 +1,10 @@
 package com.cg.accessmodifiers;
 
 public class privateaccessmodifiers1 {
+	public static void main(String[] args) {
+		privateaccessmodifiers t1=new privateaccessmodifiers();
+		t1.show();
+		System.out.println(t1.i);
+	}
 
 }
